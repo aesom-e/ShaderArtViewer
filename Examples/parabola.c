0620c64a2156b24af7559dfc9e5df69e3869237e
Evan <114953191+aesom-e@users.noreply.github.com>
2024-05-03 16:00:57 -0400

    Update parabola.c
    Updated to add the option to set the parabola's start position

--- Examples/parabola.c
+++ Examples/parabola.c
@@ -8,6 +8,7 @@
 struct {
     float a;
     char useTimeForA;
+    char startPos; // 0 = bottom, 1 = center, 2 = top
 } customFlags = {0};
 
 // Gets custom command line flags. Use --[flag] for custom flags
@@ -24,7 +25,8 @@ void getCustomFlags(LPWSTR commandLine) {
 
         if(strcmp("--help", argument) == 0) {
             printf("Program arguments:\n");
-            printf("a (float) [Default 1] | Set's the 'a' value of the parabola (y=ax^2)\n");
+            printf("a (float) [Default 1]           | Sets the 'a' value of the parabola (y=ax^2)\n");
+            printf("start (custom) [Default bottom] | Sets the position of the pixel at the start of the parabola (bottom, center, top)\n");
             exit(0);
         }
 
@@ -35,9 +37,16 @@ void getCustomFlags(LPWSTR commandLine) {
                 temp = (float)atof(argument);
                 if(temp != 0) customFlags.a = temp;
                 break;
+            case 2:
+                tempArg = 0;
+                if(strcmp(argument, "bottom") == 0) customFlags.startPos = 0;
+                if(strcmp(argument, "center") == 0) customFlags.startPos = 1;
+                if(strcmp(argument, "top") == 0)    customFlags.startPos = 2;
+                break;
         }
 
         if(strcmp("--a", argument) == 0) tempArg = 1;
+        if(strcmp("--start", argument) == 0) tempArg = 2;
 
         free(argument);
     }
@@ -46,8 +55,21 @@ void getCustomFlags(LPWSTR commandLine) {
 pixelColour shade(flags programFlags, pixel screenPixel, float t) {
     pixelColour returnColour = {0};
 
-    // -30 because that's the height of the header
-    pixel rootPixel = {getCenter(programFlags).x, programFlags.screenHeight-30};
+    pixel rootPixel;
+
+    switch(customFlags.startPos) {
+        case 1:
+            rootPixel = getCenter(programFlags);
+            break;
+        case 2:
+            rootPixel.x = getCenter(programFlags).x;
+            rootPixel.y = 0;
+            break;
+        default:
+            rootPixel.x = getCenter(programFlags).x;
+            rootPixel.y = programFlags.screenHeight-30; // -30 because that's the height of the header
+            break;
+    }
 
     // Float to match a and so I don't have to cast everything in the if
     float x = (float)(rootPixel.x - screenPixel.x), y = (float)(rootPixel.y - screenPixel.y);
